0823f852e789221badd70de94e6be6c26ed06965
Suraj Rajendra Raut <rautsuraj345@gmail.com>
2023-12-03 18:07:48 +0530

    Created : Calender Click new Meeting Page is Loads

--- src/App.js
+++ src/App.js
@@ -2,34 +2,35 @@
 import './App.css';
 import Navbar from './Navbar/Navbar';
 import TeamsActivity from './TeamsNavigation/TeamsActivity';
-// import UserMassage from './chat/messageBlock/userMessage/UserMassage';
-// import Contacts from './chat/contactBlock/Contacts';
-// import Chat from './chat/chat_main_page';
-// import Activity from './activity/activity';
-// import CalendarNavbar from './calender/navbar/CalendarNavbar';
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Routes,
-// } from "react-router-dom";
+import Chat from './chat/chat_main_page';
+import Activity from './activity/activity';
+import CalendarNavbar from './calender/navbar/CalendarNavbar';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import UserMassage from './chat/messageBlock/userMessage/UserMassage';
+import Contacts from './chat/contactBlock/Contacts';
 import NewMeeting from './calender/meeting/NewMeeting';
 
 function App() {
   return (
-    // <Router>
+    <Router>
       <div className="App"> 
         <Navbar/>
         <div className="userSection">
           <TeamsActivity/>
-          <NewMeeting/>
-          {/* <Routes>
+          {/* <NewMeeting/> */}
+          <Routes>
             <Route exact path='/' element={<Chat/>}></Route>
             <Route exact path='/calendar' element={<CalendarNavbar/>}></Route>
             <Route exact path='/activity' element={<Activity/>}></Route>
-          </Routes>   */}
+          </Routes>  
         </div>
       </div>
-    // </Router>  
+     </Router>  
   );
 }
 
