1af0cd35ca3e7104aa098557dd0b6832429b045d
Suraj Raut <surajraut347@gmail.com>
2024-05-29 15:29:56 +0530

    Updated responsive design of chat for mobile devices

--- src/TeamsNavigation/TeamsActivity.js
+++ src/TeamsNavigation/TeamsActivity.js
@@ -2,40 +2,40 @@ import React, {useState } from 'react'
 import './teamsactivity.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faCalendarDays, faCommentDots, faPeopleGroup, faBell, faCircleQuestion } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function TeamsActivity() {
-  const[selected, setSelected] = useState("chat")
-
+  let selectionPath = useLocation();
+  const[selected, setSelected] = useState(selectionPath.pathname)
   const combinedEvent = (event) =>{
-    setSelected(event[0]);
+    setSelected(event[1]);
   }
 
 
   return (
     <div className='activitySection'>
       <Link to="/activity" >
-        <div className={`activity ${selected === 'bell' ? 'selected' : ''}`} onClick={()=>combinedEvent(["bell", "/bell"])}>
+        <div className={`activity ${selected === '/bell' ? 'selected' : ''}`} onClick={()=>combinedEvent(["bell", "/bell"])}>
           <FontAwesomeIcon id="icons" className="bell" icon={faBell} />
           <p>Activity</p>
         </div>
       </Link>
 
       <Link to="/community">
-        <div className={`activity ${selected === 'community' ? 'selected' : ''}`} onClick={()=>combinedEvent(["community", "/community"])}>
+        <div className={`activity ${selected === '/community' ? 'selected' : ''}`} onClick={()=>combinedEvent(["community", "/community"])}>
           <FontAwesomeIcon id="icons" className="community" icon={faPeopleGroup} />
           <p>Community</p>
         </div>
         </Link>
       <Link to="/" >
-        <div className={`activity ${selected === 'chat' ? 'selected' : ''}`} onClick={()=>combinedEvent(["chat", "/chat"])}>
+        <div className={`activity ${selected === '/chat' ? 'selected' : ''}`} onClick={()=>combinedEvent(["chat", "/chat"])}>
           <FontAwesomeIcon id="icons" className="chat" icon={faCommentDots} />
           <p>Chat</p>
         </div>
       </Link>
 
       <Link to="/calendar" >
-        <div className={`activity ${selected === 'calendar' ? 'selected' : ''}`} onClick={()=>combinedEvent(["calendar", "/calendar"])}>
+        <div className={`activity ${selected === '/calendar' ? 'selected' : ''}`} onClick={()=>combinedEvent(["calendar", "/calendar"])}>
           <FontAwesomeIcon id="icons" className="calendar" icon={faCalendarDays} />
           <p>Calendar</p>
         </div>
